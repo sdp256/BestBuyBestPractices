074f51caba2e8a2940a13b477d9ebb58f6054266
sdp256 <steevnpairy@gmail.com>
2021-05-21 17:29:36 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace BestBuyBestPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
